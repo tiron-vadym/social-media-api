f01e704f1bb09735e7652342c4733b734cfa8fbc
Vadym Tiron <tironvadim1@gmail.com>
2024-03-21 15:39:47 +0200

    add url patterns to project

---
+++ user/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from user.views import (
+    CreateUserView,
+    LoginUserView,
+    ManageUserView,
+    logout_view,
+    FollowingViewSet,
+    FollowersViewSet,
+    PostListCreateRetrieveView,
+)
+
+router = DefaultRouter()
+router.register(r"following", FollowingViewSet, basename="following")
+router.register(r"followers", FollowersViewSet, basename="followers")
+router.register(r"profiles", ManageUserView, basename="profile")
+router.register(r"posts", PostListCreateRetrieveView, basename="post")
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", LoginUserView.as_view(), name="token"),
+    path("logout/", logout_view, name="logout"),
+    path("", include(router.urls)),
+]
+
+app_name = "user"
